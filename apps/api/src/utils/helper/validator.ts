a5f5fd24f886e40c92b83a846b8c32e8876d1441
JyrwaJR <jyrwaboys@gmail.com>
2026-03-07 23:36:28 +0530

    fix errors handle

--- apps/api/src/utils/helper/validator.ts
+++ apps/api/src/utils/helper/validator.ts
@@ -2,7 +2,7 @@ import { Hook } from "@hono/zod-validator";
 import { Context } from "hono";
 
 export const validatorHook: Hook<any, any, any, any> = (result, c: Context) => {
- if (!result.success) {
-  throw result.error;
- }
+  if (!result.success) {
+    throw (result as { error: unknown }).error;
+  }
 };
