adc44451bd9fd05c77a6eedb06813f654bc77e8a
JyrwaJR <jyrwaboys@gmail.com>
2026-03-11 23:16:30 +0530

    add validiation for logs

--- packages/validators/src/common.ts
+++ packages/validators/src/common.ts
@@ -8,3 +8,10 @@ export const passwordSchema = z
   .regex(/[A-Z]/, "Must contain an uppercase letter")
   .regex(/\d/, "Must contain a number")
   .regex(/[@$!%*?&]/, "Must contain a special character");
+
+export const logSchema = z.object({
+  type: z.enum(["ERROR", "WARN", "INFO", "LOG"]),
+  message: z.string().min(1, "Message is required"),
+  content: z.any().optional(), // Can be string, object, array, etc.
+  isBackend: z.boolean().default(false),
+});
--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -1,2 +1,3 @@
 export * from './auth';
 export * from './vault';
+export * from './common';
