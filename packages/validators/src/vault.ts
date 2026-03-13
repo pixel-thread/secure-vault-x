a5a4263836f5372b65fe363e0ad5ad87240d0845
JyrwaJR <jyrwaboys@gmail.com>
2026-03-13 20:38:05 +0530

    Validiation message

--- packages/validators/src/vault.ts
+++ packages/validators/src/vault.ts
@@ -60,20 +60,3 @@ export const syncPushSchema = z.object({
 export const syncPullSchema = z.object({
   since: z.union([z.string().datetime(), z.string().regex(/^\d+$/)]).optional(),
 });
-
-export const syncItemSchema = z.object({
-  id: z.string().uuid().optional(),
-  encryptedData: z.string().min(1, "Vault blob cannot be empty"),
-  iv: z.string().optional(),
-  version: z.number().int().positive(),
-  updatedAt: z.union([z.string().datetime(), z.number()]),
-  deletedAt: z.union([z.string().datetime(), z.number()]).nullable().optional(),
-});
-
-export const syncPushSchema = z.object({
-  items: z.array(syncItemSchema),
-});
-
-export const syncPullSchema = z.object({
-  since: z.union([z.string().datetime(), z.string().regex(/^\d+$/)]).optional(),
-});
