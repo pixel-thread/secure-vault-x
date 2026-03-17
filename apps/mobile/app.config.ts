ed7b6c6fc0fce90d755ef74e55586f9e4a558a43
JyrwaJR <jyrwaboys@gmail.com>
2026-03-17 17:18:09 +0530

    convert the app json to appconfic.ts

---
+++ apps/mobile/app.config.ts
@@ -0,0 +1,81 @@
+import { ExpoConfig, ConfigContext } from 'expo/config';
+
+const APP_VARIANT = process.env.APP_VARIANT || 'production';
+
+const getAppConfig = (variant: string) => {
+  switch (variant) {
+    case 'development':
+      return {
+        name: 'Secure Vault X (Dev)',
+        identifier: 'com.pixelthread.securevaultx.dev',
+      };
+    case 'preview':
+      return {
+        name: 'Secure Vault X (Preview)',
+        identifier: 'com.pixelthread.securevaultx.preview',
+      };
+    default:
+      return {
+        name: 'Secure Vault X',
+        identifier: 'com.pixelthread.securevaultx',
+      };
+  }
+};
+
+const configVars = getAppConfig(APP_VARIANT);
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: configVars.name,
+  slug: 'secure-vault-x',
+  version: '0.0.7',
+  scheme: ['securevaultx'],
+  platforms: ['ios', 'android'],
+  newArchEnabled: true,
+  jsEngine: 'hermes',
+  web: {
+    bundler: 'metro',
+    output: 'static',
+    favicon: './assets/favicon.png',
+  },
+  plugins: ['expo-router', 'expo-secure-store'],
+  experiments: {
+    typedRoutes: true,
+    tsconfigPaths: true,
+  },
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'automatic',
+  splash: {
+    image: './assets/splashscreen/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#001010',
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: configVars.identifier,
+    buildNumber: '7',
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#001010',
+    },
+    package: configVars.identifier,
+    versionCode: 7,
+  },
+  extra: {
+    router: {},
+    eas: {
+      projectId: 'b09a2c96-90ae-4795-b42f-c88731de08ee',
+    },
+  },
+  owner: 'pixel-thread',
+  runtimeVersion: {
+    policy: 'appVersion',
+  },
+  updates: {
+    url: 'https://u.expo.dev/b09a2c96-90ae-4795-b42f-c88731de08ee',
+  },
+});
