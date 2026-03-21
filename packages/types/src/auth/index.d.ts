2b72c7f320e8ff3202c82a5a1c20cadeef2f497d
JyrwaJR <jyrwaboys@gmail.com>
2026-03-21 15:29:49 +0530

    Fix And centralize typescrip timport

--- packages/types/src/auth/index.d.ts
+++
@@ -1,7 +0,0 @@
-export type UserT = {
-    id: string;
-    isMfaEnable: boolean;
-    email: string;
-    createdAt: string;
-};
-//# sourceMappingURL=index.d.ts.map
--- packages/types/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-export type BaseType = string;
-export type EndpointT<T extends string> = Record<T, string>;
-export * from "./api";
-export * from "./auth";
-export * from "./secret";
-//# sourceMappingURL=index.d.ts.map
